2630fb23ece60338e44b7f4d205087d60b007aa1
Luiz Lima - <luizlyma@live.com>
2022-04-27 07:59:42 -0300

    Adicionado Desafio 49

---
+++ Desafios/Desafio_49.py
@@ -0,0 +1,34 @@
+# Refaça o DESAFIO 9, mostrando a tabuada de um número que o usuário escolher, só que agora utilizando um laço for.
+
+'''
+Desafio 09:
+
+n = int(input("Digite um valor inteiro: "))
+print("A tabuada de {} é:".format(n))
+print("{}  x {:2} = {:2}".format(n,1,n*1))
+print("{}  x {:2} = {:2}".format(n,2,n*2))
+print("{}  x {:2} = {:2}".format(n,3,n*3))
+print("{}  x {:2} = {:2}".format(n,4,n*4))
+print("{}  x {:2} = {:2}".format(n,5,n*5))
+print("{}  x {:2} = {:2}".format(n,6,n*6))
+print("{}  x {:2} = {:2}".format(n,7,n*7))
+print("{}  x {:2} = {:2}".format(n,8,n*8))
+print("{}  x {:2} = {:2}".format(n,9,n*9))
+print("{}  x {:2} = {:2}".format(n,10,n*10))
+'''
+
+print('=-'*15)
+print('   TABUADA DE MULTIPLICAÇÃO')
+print('=-'*15)
+
+n = int(input('\nDigite um valor para saber a tabuada: '))
+contador = 0
+acumulador = 0
+
+print(' ')
+
+for tabuada in range(1,11):
+    contador = contador + 1
+    resultado = n * contador
+    print('{}  x {:2} = {:2}'.format(n, contador, resultado))
+    
